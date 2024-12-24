2e896843a55668af7fe5669f722762c7e35e6751
slashtechno <77907286+slashtechno@users.noreply.github.com>
2024-12-23 20:05:03 -0500

    feat: switch to static adapter

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,12 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter({
+			// https://khromov.se/the-missing-guide-to-understanding-adapter-static-in-sveltekit/
+			// npm run build && npx http-server ./build
+			pages: 'build',
+			assets: 'build',
+			fallback: "404.html",
+			precompress: false,
+			strict: true
+		}),
+		alias: {
+            $lib: 'src/lib'
+        }
 	}
 };
 
