cc3c4bd714e63ce63ac75278674334b14a75a9b0
slashtechno <77907286+slashtechno@users.noreply.github.com>
2025-02-21 19:01:45 -0600

    feat: update Collapse component to use checkbox
    The button was triggering if a child component was interacted with

--- frontend/src/lib/components/Collapse.svelte
+++ frontend/src/lib/components/Collapse.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
-    import type { Snippet } from "svelte";
+  import type { Snippet } from "svelte";
 
   let { title, children }: { title: string, children: Snippet } = $props();
+
+  function stopPropagation(event: Event) {
+    event.stopPropagation();
+  }
 </script>
 
-<button class="collapse bg-base-200 collapse-arrow" >
-  <div class="collapse-title text-xl font-medium">
-    {title}
+<div class="collapse bg-base-200 text-center collapse-arrow">
+    <input type="checkbox" />
+    <div class="collapse-title text-xl font-medium">{title}</div>
+    <div class="collapse-content">
+      {@render children?.()}
+    </div>
   </div>
-  <div class="collapse-content">
-    {@render children?.()}
-</div>
-</button>
